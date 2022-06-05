442fd8bb81aa323a8d91ffacfe838b0131f8660d
matto0O <mateusz.wozniak.0211@gmail.com>
2022-06-05 20:20:07 +0200

    Completed work on first part of the project

--- fetch_data.py
+++ fetch_data.py
@@ -4,17 +4,80 @@ import os
 import shutil
 import xml.etree.ElementTree as ET
 from dataclasses import dataclass
+import datetime
 
 stops = list()
 lines = list()
 
 
 @dataclass(frozen=True)
+class Line:
+    signature: str
+    variants: list
+
+    def __repr__(self):
+        return "Linia {}\n{}".format(self.signature, self.variants[0])
+
+    def __str__(self):
+        return f"Linia {self.signature}"
+
+
+@dataclass(frozen=True)
+class Departure:
+    line: Line
+    time: datetime.time
+    timetable: int
+    low: bool = False
+
+    def __repr__(self):
+        match self.timetable:
+            case 0:
+                days = 'dni robocze'
+            case 1:
+                days = 'sobota'
+            case _:
+                days = 'święta'
+        result = f"{self.line.__str__()}\n{self.time} - {days}"
+        if self.low:
+            result += ", niskopodłogowy"
+        return result
+
+
 class Stop:
-    code: str
-    name: str
-    latitude: str
-    longitude: str
+    def __init__(self, code: str, name: str, latitude: str, longitude: str):
+        self.__code = code
+        self.__name = name
+        self.__latitude = latitude
+        self.__longitude = longitude
+        self.departures = list()
+
+    def append_departure(self, departure: Departure):
+        self.departures.append(departure)
+
+    @property
+    def code(self):
+        return self.__code
+
+    @property
+    def name(self):
+        return self.__name
+
+    @property
+    def latitude(self):
+        return self.__latitude
+
+    @property
+    def longitude(self):
+        return self.__longitude
+
+    def __eq__(self, other):
+        if other.__class__ is self.__class__:
+            return self.__code == other.__code
+        else:
+            return NotImplementedError
+
+    def __hash__(self):
+        return hash(self.__code)
 
     def __repr__(self):
         return "{} - {} - {}, {}".format(self.code, self.name, self.latitude, self.longitude)
@@ -33,19 +96,17 @@ class Variant:
         return result[:-3]
 
 
-@dataclass(frozen=True)
-class Line:
-    signature: str
-    variants: list
-
-    def __repr__(self):
-        return "Linia {}\n{}".format(self.signature, self.variants[0])
-
-
 def find_stop_by_id(code):
     for stop in stops:
         if stop.code == code:
             return stop
+    return None
+
+
+def find_line_by_signature(signature):
+    for line in lines:
+        if line.signature == signature:
+            return line
     return None
 
 
@@ -84,7 +145,39 @@ def fetch_lines():
         lines.append(Line(root[0].attrib['nazwa'], variants))
 
 
+def fetch_departures():
+    for file in os.listdir("XML-rozkladyjazdy"):
+        tree = ET.parse(f"XML-rozkladyjazdy\{file}")
+        line = tree.getroot()
+        for variant in line[0]:
+            for stop in variant:
+                current_stop = find_stop_by_id(stop.attrib['id'])
+                try:
+                    for day in stop[1]:
+                        match day.attrib['nazwa']:
+                            case 'w dni robocze':
+                                timetable = 0
+                            case 'Sobota':
+                                timetable = 1
+                            case _:
+                                timetable = 2
+                        for hour in day:
+                            h = hour.attrib['h']
+                            for minute in hour:
+                                m = minute.attrib['m']
+                                low = False
+                                if len(minute.attrib) > 1:
+                                    low = minute.attrib['ozn'] == 'N'
+                                d = Departure(find_line_by_signature(line[0].attrib['nazwa']),
+                                            datetime.time(int(h % 24), int(m % 60)), timetable, low)
+                                # TODO consider departures post-midnight
+                                current_stop.append_departure(d)
+                except IndexError:
+                    pass
+
+
 def fetch_all():
     # unpack_timetable()
     fetch_stops()
     fetch_lines()
+    fetch_departures()
--- main.py
+++ main.py
@@ -2,6 +2,3 @@ import fetch_data
 
 if __name__ == '__main__':
     fetch_data.fetch_all()
-    for line in fetch_data.lines:
-        print(line)
-        print("\n")
