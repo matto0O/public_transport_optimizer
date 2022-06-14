d16c5d460ca2e1dcfc8db84de1bc2afa0a664f3e
matto0O <mateusz.wozniak.0211@gmail.com>
2022-06-14 02:06:02 +0200

    Added a chart to optimizer

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-import fetch_data
 from data_processing import find_connections
 from optimizer import ticket_price_to_ride_time_ratio
 
 if __name__ == '__main__':
-    # fetch_data.fetch_all()
     for ride in find_connections(7, 0, 0, "Kamienna 145, Wrocław", "D-4 PWr", 700):
         print(ride)
-    print(ticket_price_to_ride_time_ratio())
+    print(ticket_price_to_ride_time_ratio(max_trip_length=16))
--- optimizer.py
+++ optimizer.py
@@ -1,7 +1,9 @@
+import numpy as np
 from scipy import optimize
+import matplotlib.pyplot as plt
 
 
-def ticket_price_to_ride_time_ratio(time=20, normal=True, min_trip_length=0, max_trip_length=18):
+def ticket_price_to_ride_time_ratio(time=20, normal=True, min_trip_length=1, max_trip_length=100):
     def time_to_price_radio(x):
         if x < 15:
             price = 1.6
@@ -22,5 +24,13 @@ def ticket_price_to_ride_time_ratio(time=20, normal=True, min_trip_length=0, max
     constraints = [{'type': 'ineq', 'fun': lower_time_constraint},
                    {'type': 'ineq', 'fun': upper_time_constraint}]
 
+    arg = np.linspace(min_trip_length, max_trip_length)
+    y = tuple(map(lambda x: time_to_price_radio(x), arg))
+    plt.plot(arg, y)
+    plt.xlabel("Ride length in minutes")
+    plt.ylabel("Ticket cost per minute")
+    plt.title("Ticket cost per ride length")
+    plt.show()
+
     return f"Best value for money is represented when traveling for " \
-           f"{round(optimize.minimize(time_to_price_radio, x0=time, constraints=constraints).x[0])} minutes"
+           f"{round(optimize.minimize(time_to_price_radio, x0=time, method='SLSQP', constraints=constraints).x[0])} minutes"
