8a260b5be497fd4b33137a307a7c8057502ff5a6
matto0O <mateusz.wozniak.0211@gmail.com>
2022-06-14 02:21:34 +0200

    Re-added data fetching invocation into main

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from data_processing import find_connections
 from optimizer import ticket_price_to_ride_time_ratio
+import fetch_data
 
 if __name__ == '__main__':
+    fetch_data.fetch_all()
     for ride in find_connections(7, 0, 0, "Kamienna 145, Wrocław", "D-4 PWr", 700):
         print(ride)
     print(ticket_price_to_ride_time_ratio(max_trip_length=16))
